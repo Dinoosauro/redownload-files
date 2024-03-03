093e45e96316572b06f179ae9f5edd58148b0edb
Dinoosauro <80783030+Dinoosauro@users.noreply.github.com>
2024-03-03 15:02:58 +0100

    Offline support
    - A new general service worker handles both caching and fake URLs for file streams
       * This website is now usable offline
       * StreamSaver.js is no longer necessary (but the zip-stream script is still used)

---
+++ public/service-worker.js
@@ -0,0 +1,66 @@
+const cacheName = 'redownloadfiles-cache';
+const filestoCache = [
+    './',
+    './index.html',
+    './assets/index.css',
+    './assets/index.js',
+    './icon.png',
+    './manifest.json',
+    'https://cdn.jsdelivr.net/npm/streamsaver@2.0.6/examples/zip-stream.js',
+];
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll(filestoCache))
+    );
+});
+let msgSend = new BroadcastChannel("URL");
+let requestRef = [];
+let stream = {};
+self.addEventListener("message", (msg) => {
+    console.log(msg);
+    let file = msg.data;
+    if (file.action === "StreamConversion") { // A stream has been sent instead of a file. 
+        stream = file; // Save it in the stream object.
+        msgSend.postMessage({ status: "DownloadReady", newUrl: stream.url }); // Trigger the event for the download of the stream on the main thread
+        return;
+    }
+    let url = `${file.url}/${(file.id ?? "") !== "" ? `${file.id}/` : ""}${encodeURIComponent(file.file.name)}`; // Create a fake URL that'll be used to download the file.
+    requestRef.findIndex(e => e.url === url) !== -1 ? requestRef[requestRef.findIndex(e => e.url === url)].file = file.file : requestRef.push({ url: url, file: msg.data.file }); // If the URL already exists, just replace the file with the newly-provided. Otherwise, push the new file to the array.
+    msgSend.postMessage({ newUrl: url, for: msg.data.file }) // Send to the main thread information about the new link
+})
+self.addEventListener('activate', e => self.clients.claim());
+
+self.addEventListener('fetch', async (event) => {
+    const req = event.request;
+    if (requestRef.findIndex(e => e.url === req.url) !== -1 || req.url === stream.url) { // Create a fake response since a file needs to be downloaded
+        let current = req.url === stream.url ? { file: { stream: () => { return stream.stream }, size: stream.size, name: stream.name } } : requestRef.find(e => e.url === req.url); // Create a FileLike object if a stream must be downloaded (only with the essential properties used), otherwise just find the file.
+        console.log(current);
+        const responseHeaders = new Headers({ // Borrowed from StreamSaver.js.
+            'Content-Type': 'application/octet-stream; charset=utf-8',
+            'Content-Security-Policy': "default-src 'none'",
+            'X-Content-Security-Policy': "default-src 'none'",
+            'X-WebKit-CSP': "default-src 'none'",
+            'X-XSS-Protection': '1; mode=block',
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+            'Content-Disposition': `attachment; filename*=UTF-8'${encodeURIComponent(current.file.name).replace(/['()]/g, escape).replace(/\*/g, '%2A')}'`,
+            'Content-Length': current.file.size
+        })
+        let res = new Response(current.file.stream(), { headers: responseHeaders }); // Create the new response with the stream
+        event.respondWith(res);
+        return res;
+    } else if (req.url.indexOf("updatecode") !== -1) return fetch(req); else event.respondWith(networkFirst(req));
+});
+
+async function networkFirst(req) {
+    try {
+        const networkResponse = await fetch(req);
+        const cache = await caches.open('redownloadfiles-cache');
+        await cache.delete(req);
+        await cache.put(req, networkResponse.clone());
+        return networkResponse;
+    } catch (error) {
+        const cachedResponse = await caches.match(req);
+        return cachedResponse;
+    }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { join } from "node:path";
+import { buildSync } from "esbuild";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    manifest: {
-      // caches the assets/icons mentioned (assets/* includes all the assets present in your src/ directory) 
-      name: 'Redownload Files',
-      short_name: 'Redownload Files',
-      start_url: '/',
-      background_color: '#212529',
-      theme_color: '#0d6efd',
-      icons: [
-        {
-          src: '/icon.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    },
-    workbox: {
-      // defining cached files formats
-      globPatterns: ["**\/*.{js,css,html,png}"],
-      runtimeCaching: [{
-        urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'jsdelivr-static-cache',
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          },
-        }
-      }],
-      cleanupOutdatedCaches: true, // delete old caches
-      skipWaiting: true, // skip waiting phase
-      clientsClaim: true // claim clients immediately
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
     }
-  })
-  ],
+  }
 })
