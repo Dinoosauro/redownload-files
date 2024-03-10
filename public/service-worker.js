16032d4412592e9a3c66166c3a83f3cb972cd397
Dinoosauro <80783030+Dinoosauro@users.noreply.github.com>
2024-03-10 10:22:36 +0100

    Replace file name value (#4)
    * Replace file name value
    
    The user now can now delete parts of the output file name

--- public/service-worker.js
+++ public/service-worker.js
@@ -25,8 +25,12 @@ self.addEventListener("message", (msg) => {
         msgSend.postMessage({ status: "DownloadReady", newUrl: stream.url }); // Trigger the event for the download of the stream on the main thread
         return;
     }
-    let url = `${file.url}/${(file.id ?? "") !== "" ? `${file.id}/` : ""}${encodeURIComponent(file.file.name)}`; // Create a fake URL that'll be used to download the file.
-    requestRef.findIndex(e => e.url === url) !== -1 ? requestRef[requestRef.findIndex(e => e.url === url)].file = file.file : requestRef.push({ url: url, file: msg.data.file }); // If the URL already exists, just replace the file with the newly-provided. Otherwise, push the new file to the array.
+    let newName = msg.data.file.name.replaceAll((msg.data.replace ?? ""), ""); // If the user wants to replace a part of the file name, do it now.
+    let url = `${file.url}/${(file.id ?? "") !== "" ? `${file.id}/` : ""}${encodeURIComponent(newName)}`; // Create a fake URL that'll be used to download the file.
+    if (requestRef.findIndex(e => e.url === url) !== -1) {
+        requestRef[requestRef.findIndex(e => e.url === url)].file = file.file;
+        requestRef[requestRef.findIndex(e => e.url === url)].name = newName;
+    } else requestRef.push({ url: url, file: msg.data.file, name: newName }); // If the URL already exists, just replace the file with the newly-provided. Otherwise, push the new file to the array.
     msgSend.postMessage({ newUrl: url, for: msg.data.file }) // Send to the main thread information about the new link
 })
 self.addEventListener('activate', e => self.clients.claim());
@@ -34,7 +38,7 @@ self.addEventListener('activate', e => self.clients.claim());
 self.addEventListener('fetch', async (event) => {
     const req = event.request;
     if (requestRef.findIndex(e => e.url === req.url) !== -1 || req.url === stream.url) { // Create a fake response since a file needs to be downloaded
-        let current = req.url === stream.url ? { file: { stream: () => { return stream.stream }, size: stream.size, name: stream.name } } : requestRef.find(e => e.url === req.url); // Create a FileLike object if a stream must be downloaded (only with the essential properties used), otherwise just find the file.
+        let current = req.url === stream.url ? { file: { stream: () => { return stream.stream }, size: stream.size }, name: stream.name } : requestRef.find(e => e.url === req.url); // Create a FileLike object if a stream must be downloaded (only with the essential properties used), otherwise just find the file.
         console.log(current);
         const responseHeaders = new Headers({ // Borrowed from StreamSaver.js.
             'Content-Type': 'application/octet-stream; charset=utf-8',
@@ -43,9 +47,10 @@ self.addEventListener('fetch', async (event) => {
             'X-WebKit-CSP': "default-src 'none'",
             'X-XSS-Protection': '1; mode=block',
             'Cross-Origin-Embedder-Policy': 'require-corp',
-            'Content-Disposition': `attachment; filename*=UTF-8'${encodeURIComponent(current.file.name).replace(/['()]/g, escape).replace(/\*/g, '%2A')}'`,
+            'Content-Disposition': `attachment; filename*=UTF-8'${encodeURIComponent(current.name).replace(/['()]/g, escape).replace(/\*/g, '%2A')}'`,
             'Content-Length': current.file.size
-        })
+        });
+        console.log(current.name);
         let res = new Response(current.file.stream(), { headers: responseHeaders }); // Create the new response with the stream
         event.respondWith(res);
         return res;
